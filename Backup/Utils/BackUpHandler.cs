cfa9e8632457643b021984a25185cd931af96784
EvgenKoulik <ursor63@mail.ru>
2022-08-08 00:47:44 +0300

    Add logging

--- Backup/Utils/BackUpHandler.cs
+++ Backup/Utils/BackUpHandler.cs
@@ -11,8 +11,13 @@ namespace Backup
         {
             var currentDateTime = DateTime.Now.ToString(settings.DateTimeFormat);
             var targetFolder = Path.Combine(settings.TargetFolder, currentDateTime);
-            _log = new BackUpLogger(targetFolder);
-
+            if (!Directory.Exists(settings.TargetFolder))
+            {
+                Console.WriteLine("Destination folder not found!");
+                return;
+            }
+            _log = new BackUpLogger(settings.TargetFolder);
+            
             foreach (var currentSourceFolder in settings.SourceFolders)
             {
                 if (Directory.Exists(currentSourceFolder))
@@ -21,17 +26,15 @@ namespace Backup
                     {
                         CopyFolder(currentSourceFolder, targetFolder);
                     }
-                    catch (ArgumentException argumentException) {_log.Error("");}
-                    catch (NotSupportedException notSupportedException) {_log.Error("");}
-                    catch (SecurityException securityException) {_log.Error("");}
-                    catch (IOException ioException) {_log.Error("");}
-                    catch (UnauthorizedAccessException unauthorizedAccessException) {_log.Error("");}
+                    catch (ArgumentException argumentException) {_log.Error(argumentException.Message);}
+                    catch (NotSupportedException notSupportedException) {_log.Error(notSupportedException.Message);}
+                    catch (SecurityException securityException) {_log.Error(securityException.Message);}
+                    catch (IOException ioException) {_log.Error(ioException.Message);}
+                    catch (UnauthorizedAccessException unauthorizedAccessException) {_log.Error(unauthorizedAccessException.Message);}
                 }
                 else
                 {
-                    _log.Error("");
-                    _log.Debug("");
-                    _log.Info("");
+                    _log.Error($"File {currentSourceFolder} not exist!");
                 }
             }
         }
@@ -40,27 +43,27 @@ namespace Backup
         {
             if (!Directory.Exists(destFolder))
             {
-                _log!.Debug("");
+                _log!.Debug($"Trying to create a {destFolder} directory");
                 Directory.CreateDirectory(destFolder);
-                _log!.Info("");
+                _log!.Info($"Directory {destFolder} successfully created");
             }
             
             var files = Directory.GetFiles(sourceFolder);
 
             foreach (var file in files)
             {
-                _log!.Debug("");
+                _log!.Debug($"Trying to copy a {file} in {destFolder} directory");
                 File.Copy(file, Path.Combine(destFolder, Path.GetFileName(file)));
-                _log!.Info("");
+                _log!.Info($"File {file} copied successfully to {destFolder} folder");
             }
             
             var folders = Directory.GetDirectories(sourceFolder);
 
             foreach (var folder in folders)
             {
-                _log!.Debug("");
+                _log!.Debug($"Trying to copy from {folder} to {destFolder} directory");
                 CopyFolder(folder, Path.Combine(destFolder, Path.GetFileName(folder)));
-                _log!.Info("");
+                _log!.Info($"Folder {folder} successfully copied to {destFolder} folder");
             }
         }
     }
--- Backup/Utils/BackUpLogger.cs
+++ Backup/Utils/BackUpLogger.cs
@@ -1,8 +1,9 @@
 ﻿namespace Backup.Utils
 {
-    public class BackUpLogger
+    public class BackUpLogger 
     {
         private readonly string _pathToFile;
+        private const string FileName =  "Log.txt";
 
         public BackUpLogger(string pathToFile)
         {
@@ -11,23 +12,23 @@
         
         public void Info(string info)
         {
-            
-            
-            
+            var path = Path.Combine(_pathToFile, FileName);
+            using var writer = new StreamWriter(path, true, System.Text.Encoding.Default);
+            writer.WriteAsync($"Info:\n{info}\n");
         }
 
         public void Error(string info)
         {
-            
-            
-            
+            var path = Path.Combine(_pathToFile, FileName);
+            using var writer = new StreamWriter(path, true, System.Text.Encoding.Default);
+            writer.WriteAsync($"Error:\n{info}\n");
         }
 
         public void Debug(string info)
         {
-            
-            
-            
+            var path = Path.Combine(_pathToFile, FileName);
+            using var writer = new StreamWriter(path, true, System.Text.Encoding.Default);
+            writer.WriteAsync($"Debug:\n{info}\n");
         }
     }
 }
